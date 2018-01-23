8bdb2ff318dcd8d7579fcf59ed12634df2d9a98a
gegorov <gegorov@yahoo.com>
2018-01-24 01:07:00 +0300

    refactored index.js as a lib with common components for games

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import readlineSync from 'readline-sync';
 
+const welcome = () => 'Welcome to the Brain Games!\n';
+
 const greetings = () => {
   const actual = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${actual}!`);
 };
 
-export default greetings;
+const startGame = (rules) => {
+  const hello = welcome();
+  console.log(hello);
+  greetings();
+  console.log(rules);
+  return 0;
+};
+
+export default startGame;
