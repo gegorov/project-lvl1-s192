d64426510445374b327440b416cfc4b10355fd7d
gegorov <gegorov@yahoo.com>
2018-01-23 15:43:21 +0300

    some minor fixes in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const actual = readlineSync.question('Your answer: ');
-console.log(`Hello, ${actual}`);
+const greetings = () => {
+  const actual = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${actual}!`);
+};
+
+export default greetings;
