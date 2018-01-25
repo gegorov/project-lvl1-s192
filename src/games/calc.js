531584d4b4a6454fb518c812669ac838c2dae7ce
gegorov <gegorov@yahoo.com>
2018-01-26 00:24:43 +0300

    brain-calc game implemented,

---
+++ src/games/calc.js
@@ -0,0 +1,36 @@
+// Calc game logic
+import { cons } from 'hexlet-pairs';
+import { newGame, getRandomNumber } from '..';
+
+const calc = () => {
+  const rule = 'What is the result of the expression?';
+
+  const calculator = (operator, num1, num2) => {
+    switch (operator) {
+      case '-': return num1 - num2;
+      case '+': return num1 + num2;
+      default: return num1 * num2;
+    }
+  };
+
+  const randomOperator = () => {
+    const random = getRandomNumber(1, 4);
+    switch (random) {
+      case 1: return '-';
+      case 2: return '+';
+      default: return '*';
+    }
+  };
+
+  const num1 = getRandomNumber(1, 101);
+  const num2 = getRandomNumber(1, 101);
+  const operator = randomOperator();
+  const question = `${num1} ${operator} ${num2}`;
+  const answer = `${calculator(operator, num1, num2)}`;
+
+  return cons(rule, cons(question, answer));
+};
+
+const calcGame = () => newGame(calc);
+
+export default calcGame;
