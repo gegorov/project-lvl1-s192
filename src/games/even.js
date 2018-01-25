e5dcca5b9cd18c75a47703f409b24a70c2aa3427
gegorov <gegorov@yahoo.com>
2018-01-26 00:24:02 +0300

    modified random function

--- src/games/even.js
+++ src/games/even.js
@@ -5,7 +5,7 @@ import { newGame, getRandomNumber } from '..';
 const even = () => {
   const rule = 'Answer "yes" if number even otherwise answer "no".';
   const isEven = num => (num % 2 === 0 ? 'yes' : 'no');
-  const question = getRandomNumber();
+  const question = getRandomNumber(1, 1000);
   const answer = isEven(question);
 
   return cons(rule, cons(question, answer));
