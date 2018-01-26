08500ee296d32a3c26ee4ed4f916aa198f28d9ce
gegorov <gegorov@yahoo.com>
2018-01-26 19:41:53 +0300

    gcd game implemented

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,15 +2,29 @@
 import { cons } from 'hexlet-pairs';
 import { newGame, getRandomNumber } from '..';
 
-const gcd = () => {
-  const rule = 'Find the greatest common divisor of given numbers.';
+const rule = 'Find the greatest common divisor of given numbers.';
+const gcDivisor = () => {
+  const number1 = getRandomNumber(1, 101);
+  const number2 = getRandomNumber(1, 101);
 
-  const question = `${num1} ${num2}`;
-  const answer = `${num1} ${num2}`;
+  const gcd = (num1, num2) => {
+    const dividend = Math.max(num1, num2);
+    const divisor = Math.min(num1, num2);
+    const remainder = dividend % divisor;
+    if (remainder === 0) {
+      return divisor;
+    }
+    return gcd(divisor, remainder);
+  };
 
-  return cons(rule, cons(question, answer));
-}
+  const result = gcd(number1, number2);
 
-const gcdGame = () => newGame(gcd);
+  const question = `${number1} ${number2}`;
+  const answer = `${result}`;
+
+  return cons(question, answer);
+};
+
+const gcdGame = () => newGame(gcDivisor, rule);
 
 export default gcdGame;
