d665da57e27a89ec51738b9edeaecd68d4be522a
gegorov <gegorov@yahoo.com>
2018-01-26 18:04:48 +0300

    even refactored. answer logic separated from isEven function

--- src/games/even.js
+++ src/games/even.js
@@ -5,9 +5,9 @@ import { newGame, getRandomNumber } from '..';
 const rule = 'Answer "yes" if number even otherwise answer "no".';
 
 const even = () => {
-  const isEven = num => (num % 2 === 0 ? 'yes' : 'no');
+  const isEven = num => num % 2 === 0;
   const question = getRandomNumber(1, 1000);
-  const answer = isEven(question);
+  const answer = isEven(question) ? 'yes' : 'no';
 
   return cons(question, answer);
 };
