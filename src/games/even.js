7feae05e01adc775e201a06ddffc778bc91b1273
gegorov <gegorov@yahoo.com>
2018-01-26 17:51:16 +0300

    even refacored to be compatible with newGame

--- src/games/even.js
+++ src/games/even.js
@@ -2,15 +2,16 @@
 import { cons } from 'hexlet-pairs';
 import { newGame, getRandomNumber } from '..';
 
+const rule = 'Answer "yes" if number even otherwise answer "no".';
+
 const even = () => {
-  const rule = 'Answer "yes" if number even otherwise answer "no".';
   const isEven = num => (num % 2 === 0 ? 'yes' : 'no');
   const question = getRandomNumber(1, 1000);
   const answer = isEven(question);
 
-  return cons(rule, cons(question, answer));
+  return cons(question, answer);
 };
 
-const evenGame = () => newGame(even);
+const evenGame = () => newGame(even, rule);
 
 export default evenGame;
