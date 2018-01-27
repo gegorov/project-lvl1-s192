3280793ac43fe4e9cd36c270bd919ed185cddf6a
gegorov <gegorov@yahoo.com>
2018-01-27 18:45:29 +0300

    prime added

---
+++ src/games/prime.js
@@ -0,0 +1,17 @@
+// prime game logic
+import { cons } from 'hexlet-pairs';
+import { newGame, getRandomNumber } from '..';
+
+const rule = 'Balance the given number.';
+const prime = () => {
+  const number = getRandomNumber(1, 10);
+
+  const isPrime = num => !(Array(num + 1).join(1).match(/^1?$|^(11+?)\1+$/));
+
+  const question = `Is this number prime? ${number}`;
+  const answer = isPrime(number) ? 'yes' : 'no';
+
+  return cons(question, answer);
+};
+
+export default () => newGame(prime, rule);
